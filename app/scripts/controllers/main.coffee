f22ebe48e17249243adb24357582ba41f98f3457
Daniel Fagerstrom <daniel@danielfagerstrom.com>
2012-11-11 11:42:47 +0100

    Improved styling and added buttons for clicked locations and pan to the specific position.

--- app/scripts/controllers/main.coffee
+++ app/scripts/controllers/main.coffee
@@ -1,11 +1,11 @@
 angular.module('angularGmapApp')
   .controller 'MainCtrl', ($scope) ->
-    $scope.mapOptions =
-      center: new google.maps.LatLng(35.784, -78.670)
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-      zoom: 16
-
+    $scope.currentMapCenter = new google.maps.LatLng(35.784, -78.670)
     $scope.zoom = 15
 
+    $scope.locations = []
     $scope.addMarker = (evt) ->
-      console.log JSON.stringify evt.latLng
+      $scope.locations.push evt.latLng
+    $scope.panTo = (location) ->
+      $scope.currentMapCenter = location
+      
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,14 @@
 (function() {
 
   angular.module('angularGmapApp').controller('MainCtrl', function($scope) {
-    $scope.mapOptions = {
-      center: new google.maps.LatLng(35.784, -78.670),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoom: 16
-    };
+    $scope.currentMapCenter = new google.maps.LatLng(35.784, -78.670);
     $scope.zoom = 15;
-    return $scope.addMarker = function(evt) {
-      return console.log(JSON.stringify(evt.latLng));
+    $scope.locations = [];
+    $scope.addMarker = function(evt) {
+      return $scope.locations.push(evt.latLng);
+    };
+    return $scope.panTo = function(location) {
+      return $scope.currentMapCenter = location;
     };
   });
 
