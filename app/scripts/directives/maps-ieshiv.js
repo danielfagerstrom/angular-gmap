5ba53e43465d1b589464d467d1e822a4a641f238
Daniel Fagerstrom <daniel@danielfagerstrom.com>
2012-12-06 12:34:23 +0100

    Created an ie-shiv for the custom tag elements.

---
+++ app/scripts/directives/maps-ieshiv.js
@@ -0,0 +1,7 @@
+// needed for IE8 and below, see http://docs.angularjs.org/guide/ie
+document.createElement('gmap-map');
+document.createElement('gmap-marker');
+document.createElement('gmap-info-window');
+document.createElement('gmap-styled-marker');
+document.createElement('gmap-info-bubble');
+document.createElement('gmap-marker-clusterer');
